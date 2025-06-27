8c808e123165d562c8e9012b481005cbf3e20dc4
Jules PESIN <jules.pesin@gmail.com>
2025-06-28 00:44:07 +0200

    player movement and microwave place holder

--- scripts/plate.gd
+++ scripts/plate.gd
@@ -1,10 +1,11 @@
-extends Sprite3D
+extends Node3D
 class_name Plate
 
 @export var rotation_speed : float = 1.0
+const RADIUS = 0.5
 
 func _process(delta: float) -> void:
-	return
+	#return
 	rotate(Vector3.UP,rotation_speed * delta)
 	
 func get_angle() -> float:
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,11 +7,13 @@ var direction : Vector2
 @export var speed : float = 1
 
 func _physics_process(delta: float) -> void:
-	direction = Input.get_vector("move_left","move_right","move_down","move_up")
-	print("direction = ",direction)
+	direction = Input.get_vector("move_right","move_left","move_down","move_up")
+	#print("direction = ",direction)
 	var angle : float = plate.get_angle()
-	print("angle = ",angle)
-	var rotated_dir : Vector2 = Vector2(direction.x * sin(angle) - direction.y * cos(angle),
+	#print("angle = ",angle)
+	var rotated_dir : Vector2 = Vector2(direction.x * cos(angle) - direction.y * sin(angle),
 	direction.x * sin(angle) + direction.y * cos(angle))
-	print("rotated_dir = ",rotated_dir)
+	#print("rotated_dir = ",rotated_dir)
 	position += Vector3(rotated_dir.x,rotated_dir.y,0) * delta * speed
+	if(position.distance_to(Vector3.ZERO) > plate.RADIUS) :
+		position = position.normalized() * plate.RADIUS

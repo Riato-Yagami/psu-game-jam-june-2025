6a2e5908752f1d947a29a1ebb09c678d49a914df
Jules PESIN <jules.pesin@gmail.com>
2025-06-27 23:35:53 +0200

    player scene

--- scripts/plate.gd
+++ scripts/plate.gd
@@ -4,5 +4,8 @@ class_name Plate
 @export var rotation_speed : float = 1.0
 
 func _process(delta: float) -> void:
+	return
 	rotate(Vector3.UP,rotation_speed * delta)
 	
+func get_angle() -> float:
+	return rotation.y
---
+++ scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+class_name Player
+
+@export var plate : Plate
+
+var direction : Vector2
+@export var speed : float = 1
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("move_left","move_right","move_down","move_up")
+	print("direction = ",direction)
+	var angle : float = plate.get_angle()
+	print("angle = ",angle)
+	var rotated_dir : Vector2 = Vector2(direction.x * sin(angle) - direction.y * cos(angle),
+	direction.x * sin(angle) + direction.y * cos(angle))
+	print("rotated_dir = ",rotated_dir)
+	position += Vector3(rotated_dir.x,rotated_dir.y,0) * delta * speed

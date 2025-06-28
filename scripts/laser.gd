c7c78dfdf66dd586058620b3bcd99ef53bc77c79
Musiquammation <bryanmegherbi@gmail.com>
2025-06-28 17:16:33 +0200

    Laser.setAngle

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -6,11 +6,11 @@ class_name Laser
 @export var SPEED = 1
 
 func _init():
-	pass
-
-
-func initialize():
 	position = Vector3(0, 0, POS_Z)
 
+
 func setAngle(note):
-	pass
+	var angle = note * 7/6.28318530718
+	
+	
+	

346560ec6e884a4c8114b62ec6eaa6f0b2f34531
DernieresPensees <67500471+DernieresPensees@users.noreply.github.com>
2025-06-29 08:25:24 +0200

    Music

--- scripts/plate.gd
+++ scripts/plate.gd
@@ -12,9 +12,8 @@ class_name Plate
 
 var phase_options_count : int
 var duration_timer : float
-
-@export var reversal_duration : float
-var reversal_timer : float
+@export var transition_duration : float = 1.5
+var transition_timer : float
 
 @export var plate_dimensions : Dimensions
 @export var player : Player
@@ -25,18 +24,20 @@ var radius : float :
 	get : return plate_dimensions.width / 2 / 100 # inputed as mm
 
 func _ready():
-	SceneManager.game.on_game_start.connect(_set_random_phase)
+	SceneManager.game.on_game_start.connect(_set_phase_start)
 	phase_options_count = phase_target_speeds.size()
 	rng.randomize()  # Seeds the generator with a random value
+	
+func _set_phase_start():
+	_set_phase(0)
 
-func _set_random_phase():
-	var phaseIndex = rng.randi_range(0, phase_options_count - 1)
-	duration_timer = phase_durations[phaseIndex]
+func _set_phase(index: int):
+	duration_timer = phase_durations[index] - 0.5
 	if (target_rotation_speed > 0):
-		target_rotation_speed = - phase_target_speeds[phaseIndex]
+		target_rotation_speed = - phase_target_speeds[index]
 	else:
-		target_rotation_speed = phase_target_speeds[phaseIndex]
-	#SoundManager.play(phase_audio_strings[phaseIndex], 1, 1, duration_timer)
+		target_rotation_speed = phase_target_speeds[index]
+	SoundManager.play(phase_audio_strings[index], 1, 1, duration_timer)
 
 func _process(delta: float) -> void:
 	if (!SceneManager.game.in_game):
@@ -54,7 +55,14 @@ func _process(delta: float) -> void:
 	if (duration_timer > 0):
 		duration_timer -= delta
 		if (duration_timer <= 0):
-			_set_random_phase()
+			transition_timer = transition_duration
+			SoundManager.play("rewind", 1, 1, 0)
+			
+	if (transition_timer > 0):
+		transition_timer -= delta
+		if (transition_timer <= 0):
+			_set_phase(rng.randi_range(0, phase_options_count - 1))
+	
 	#return
 	rotate(Vector3.UP,rotation_speed * delta)
 	
